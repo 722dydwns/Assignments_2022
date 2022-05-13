435fa0e6263352b31b504e2008d4f1bb9d9584ff
YongJoOoO <39732720+YongJoOoO@users.noreply.github.com>
2022-05-13 09:43:30 +0900

    Add files via upload

---
+++ Spring_실습/demo1/src/test/java/com/example/skuniv/spring/Demo1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.skuniv.spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
